c4ce08a53dcf8d50d1ded7d59f5a69820055998d
ErinCain <ecain@flowwest.com>
2021-09-29 13:56:45 -0700

    Adds rmarkdown docs and starts data processing

--- scripts/adult-upstream-passage-monitoring/clear-creek-data-cleaning.R
+++ scripts/adult-upstream-passage-monitoring/clear-creek-data-cleaning.R
@@ -14,13 +14,26 @@ gcs_list_objects()
 # git data and save as xlsx
 gcs_get_object(object_name = "adult-upstream-passage-monitoring/clear-creek/ClearCreekVideoWeir_AdultRecruitment_2013-2020.xlsx",
                bucket = gcs_get_global_bucket(),
-               saveToDisk = "test-data.xlsx")
+               saveToDisk = "test-data.xlsx",
+               overwrite = TRUE)
 
 # read in data to clean 
-raw_data_test <- readxl::read_excel("test-data.xlsx")
+readxl::excel_sheets("test-data.xlsx")
+domain_description <- readxl::read_excel("test-data.xlsx", sheet = "Domain Description") %>% glimpse()
+raw_data_test <- readxl::read_excel("test-data.xlsx", sheet = "ClearCreekVideoWeir_AdultRecrui") %>% glimpse()
 
+unique(raw_data_test$Run)
+# TODO fix time_passed to be time format, fix time_block to be in time format
 # clean data 
+filtered_data <- raw_data_test %>% 
+  set_names(tolower(colnames(raw_data_test))) %>%
+  mutate(date = as.Date(date),
+         time_block = format(time_block, format = "%H:%M:%S")) %>% # Left in characters for now TODO change to time
+  filter(species == "CHN", run == "SR") %>% 
+  select(-stt_size) %>% glimpse()
 
+# 
 
+unique(filtered_data$Species)
 # save data back up to bucket 
 
