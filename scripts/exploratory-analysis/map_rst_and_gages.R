ce415796d9cf0af2a701485bba239afce3252365
ErinCain <ecain@flowwest.com>
2022-03-04 14:39:38 -0800

    Basic map describing RST and gages"

---
+++ scripts/exploratory-analysis/map_rst_and_gages.R
@@ -0,0 +1,55 @@
+library(tidyverse)
+library(leaflet)
+library(sp)
+
+# TODO add jitter to spread points out
+gage_sites <- read_csv("scripts/exploratory-analysis/data/gage_sites.csv") 
+
+cdec_sites <- gage_sites %>% 
+  filter(gage_agency == "CDEC") 
+
+usgs_sites <- gage_sites %>% 
+  filter(gage_agency == "USGS")  
+
+rst_sites <- read_csv("scripts/exploratory-analysis/data/rst_sites.csv") 
+leaflet(gage_sites)  %>% 
+  addProviderTiles(providers$Esri.WorldTopoMap, group = "Map") %>% 
+  addCircleMarkers(data = cdec_sites,  label = cdec_sites$gage_number, 
+                   weight = 1.5, color = "orange",
+                   opacity =  1, fillOpacity = .25, 
+                   labelOptions = labelOptions(noHide = T, # Set to F to hide labels
+                                               style = list("font-size" = "14px")), 
+                   popup = paste(sep = "<br/>",
+                                 cdec_sites$site_name,
+                                 case_when(cdec_sites$flow_gage == TRUE & 
+                                             cdec_sites$temp_gage == TRUE ~ "FLow & Temp",
+                                           cdec_sites$flow_gage == TRUE ~ "Flow",
+                                           cdec_sites$temp_gage == TRUE ~ "Temp",
+                                           TRUE ~ "No Flow or Temp Data"))
+  ) %>%
+  addCircleMarkers(data = usgs_sites,  label = usgs_sites$gage_number, 
+                   weight = 1.5, color = "green",
+                   opacity =  1, fillOpacity = .25, 
+                   labelOptions = labelOptions(noHide = T, # Set to F to hide labels
+                                               style = list("font-size" = "14px")), 
+                   popup = paste(sep = "<br/>",
+                                 usgs_sites$site_name,
+                                 case_when(usgs_sites$flow_gage == TRUE & 
+                                             usgs_sites$temp_gage == TRUE ~ "FLow & Temp",
+                                           usgs_sites$flow_gage == TRUE ~ "Flow",
+                                           usgs_sites$temp_gage == TRUE ~ "Temp",
+                                           TRUE ~ "No Flow or Temp Data"))
+  ) %>%
+  addCircleMarkers(data = rst_sites,  label = rst_sites$site_name, 
+                   weight = 1.5, color = "blue",
+                   opacity =  1, fillOpacity = .25, 
+                   labelOptions = labelOptions(noHide = T, # Set to F to hide labels
+                                               style = list("font-size" = "14px")), 
+                   popup = paste(sep = "<br/>",
+                                 rst_sites$sub_site_name,
+                                 paste("River Mile:", rst_sites$river_mile),
+                                 paste("RST Size:", rst_sites$rst_size),
+                                 paste("RST Season:", rst_sites$trapping_season))
+  ) %>%
+  addScaleBar()
+
---
+++ scripts/exploratory-analysis/prep_data.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(OSMscale)
+library(readr)
+# RST and gage locations
+rst_sites <- readxl::read_excel("scripts/exploratory-analysis/data/JPE-rst-and-gage-sites.xlsx", sheet = "rst") %>%
+  mutate(degree = degree(latitude, longitude),
+         latitude = ifelse(coordinate_units == "lat/long", degree$lat, latitude), 
+         longitude = ifelse(coordinate_units == "lat/long", degree$long, longitude)) %>%
+  select(-degree, -coordinate_units) %>% glimpse
+
+rst_sites
+write_csv(rst_sites, "scripts/exploratory-analysis/data/rst_sites.csv")
+
+gage_sites <- readxl::read_excel("scripts/exploratory-analysis/data/JPE-rst-and-gage-sites.xlsx", sheet = "gage")
+gage_sites
+write_csv(gage_sites, "scripts/exploratory-analysis/data/gage_sites.csv")
