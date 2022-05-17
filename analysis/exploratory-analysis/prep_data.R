b61482193db38df31c1053258f382a80c188edc9
ErinCain <ecain@flowwest.com>
2022-05-17 13:22:25 -0700

    Update site info for mark recapture & add yub into sites table

--- analysis/exploratory-analysis/prep_data.R
+++ analysis/exploratory-analysis/prep_data.R
@@ -1,11 +1,10 @@
 library(tidyverse)
-library(OSMscale)
 library(readr)
 # RST and gage locations
-sheets <- readxl::excel_sheets("analysis/exploratory-analysis/data/JPE-rst-and-gage-sites.xlsx")
-rst_years <- readxl::read_excel("analysis/exploratory-analysis/data/JPE-rst-and-gage-sites.xlsx", sheet = "rst_year_lookup")
+sheets <- readxl::excel_sheets("analysis/exploratory-analysis/data-raw/JPE-rst-and-gage-sites.xlsx")
+rst_years <- readxl::read_excel("analysis/exploratory-analysis/data-raw/JPE-rst-and-gage-sites.xlsx", sheet = "rst_year_lookup")
 
-rst_sites <- readxl::read_excel("analysis/exploratory-analysis/data/JPE-rst-and-gage-sites.xlsx", sheet = "rst") %>%
+rst_sites <- readxl::read_excel("analysis/exploratory-analysis/data-raw/JPE-rst-and-gage-sites.xlsx", sheet = "rst") %>%
   # mutate(
   #   degree = degree(latitude, longitude),
   #        latitude = ifelse(coordinate_units == "lat/long", degree$lat, latitude), 
@@ -15,14 +14,14 @@ rst_sites <- readxl::read_excel("analysis/exploratory-analysis/data/JPE-rst-and-
 
 rst_sites$site_name
 
-write_csv(rst_sites, "analysis/exploratory-analysis/data/rst_sites.csv")
+write_csv(rst_sites, "analysis/exploratory-analysis/data-raw/rst_sites.csv")
 
-gage_years <- readxl::read_excel("analysis/exploratory-analysis/data/JPE-rst-and-gage-sites.xlsx", sheet = "gage_year_lookup")
+gage_years <- readxl::read_excel("analysis/exploratory-analysis/data-raw/JPE-rst-and-gage-sites.xlsx", sheet = "gage_year_lookup")
 
-gage_sites <- readxl::read_excel("analysis/exploratory-analysis/data/JPE-rst-and-gage-sites.xlsx", sheet = "gage")  %>% 
+gage_sites <- readxl::read_excel("analysis/exploratory-analysis/data-raw/JPE-rst-and-gage-sites.xlsx", sheet = "gage")  %>% 
   group_by(identifier) %>% 
   filter(row_number() == 1) %>%
   left_join(gage_years) %>%
   ungroup()
 gage_sites
-write_csv(gage_sites, "analysis/exploratory-analysis/data/gage_sites.csv")
+write_csv(gage_sites, "analysis/exploratory-analysis/data-raw/gage_sites.csv")
