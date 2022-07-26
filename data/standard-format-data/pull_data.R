07abb2f491f66c588a44444f252ac1284b1be972
alvizek <avizek@flowwest.com>
2022-07-26 15:46:19 -0700

    adds pull data script

---
+++ data/standard-format-data/pull_data.R
@@ -0,0 +1,60 @@
+library(googleCloudStorageR)
+library(tidyverse)
+
+gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
+gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
+
+# Mark-Recapture Data for RST
+# standard recapture data table
+gcs_get_object(object_name = "standard-format-data/standard_recapture.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/standard_recapture.csv",
+               overwrite = TRUE)
+# standard release data table
+gcs_get_object(object_name = "standard-format-data/standard_release.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/standard_release.csv",
+               overwrite = TRUE)
+
+# RST Monitoring Data
+# standard rst catch data table
+gcs_get_object(object_name = "standard-format-data/standard_rst_catch.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/standard_catch.csv",
+               overwrite = TRUE)
+# standard rst trap data table
+gcs_get_object(object_name = "standard-format-data/rst_trap_locations.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/rst_trap_locations.csv",
+               overwrite = TRUE)
+# standard environmental covariate data collected during RST monitoring
+gcs_get_object(object_name = "standard-format-data/standard_RST_environmental.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/standard_environmental.csv",
+               overwrite = TRUE)
+# rst site data
+gcs_get_object(object_name = "standard-format-data/standard_RST_environmental.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/standard_environmental.csv",
+               overwrite = TRUE)
+
+# Standard Environmental Covariate Data
+# standard flow
+gcs_get_object(object_name = "standard-format-data/standard_flow.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/standard_flow.csv",
+               overwrite = TRUE)
+
+# TODO ADD TEMP WHEN READY
+
+# Adult Upstream Data
+gcs_get_object(object_name = "standard-format-data/standard_adult_upstream_passage.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/standard_adult_upstream.csv",
+               overwrite = TRUE)
+
+# Adult Holding Data
+gcs_get_object(object_name = "standard-format-data/standard_holding.csv",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "data/standard-format-data/standard_holding.csv",
+               overwrite = TRUE)
