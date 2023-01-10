be37bdb04bafa03b341e5259cb19d13e864be773
liz-fw <lstebbins@flowwest.com>
2023-01-10 11:45:34 -0800

    plotly edits

--- analysis/lhd_workshop.R
+++ analysis/lhd_workshop.R
@@ -20,6 +20,29 @@ daily_catch_unmarked <- read_csv("data/model-data/daily_catch_unmarked.csv")
 # we will put these in a shiny app because will be easier to navigate the many
 # plots
 
+# butte 
+butte_catch <- filter(daily_catch_unmarked, stream == "butte creek", 
+                      fork_length < 1000) |> 
+  mutate(wy = ifelse(month(date) %in% 10:12, year(date) + 1, year(date)))
+# butte - 2019
+# need to set the x-axis to start in november
+
+# set water year
+water_year = 2019
+# set max_date for xlim
+max_date = butte_catch |> filter(wy == water_year) |> 
+  summarize(max(date, na.rm = T)) |> magrittr:::extract2(1)
+
+filter(butte_catch, wy == water_year) |> 
+  ggplot(aes(x = date, y = fork_length)) +
+  geom_point() +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b",
+               limits = c(ymd(paste0(water_year-1, "-10-01")), ymd(paste(max_date)))) +
+  labs(y = "fork length (mm)",
+       x = "")
+
+
+# try plot_ly -------------------------------------------------------------
 
 water_year = 2018:2019
 selected_stream = "butte creek"
