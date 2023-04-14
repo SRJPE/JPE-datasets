9eea8b1fa3d0613a95b6005b472d45ae19c28270
liz-fw <lstebbins@flowwest.com>
2023-04-14 10:39:54 -0700

    creates file for exploring temperature for adult model

---
+++ analysis/adult_model_data_prep.R
@@ -0,0 +1,160 @@
+# pull in and prep data for adult model
+
+# libraries ---------------------------------------------------------------
+library(tidyverse)
+library(lubridate)
+library(googleCloudStorageR)
+library(rstan)
+library(bayesplot)
+
+# pull adult data & process ----------------------------------------------------------------
+gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
+gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
+
+# upstream passage total counts
+upstream_passage <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_adult_upstream_passage.csv",
+                                            bucket = gcs_get_global_bucket())) |>
+  filter(!is.na(date)) |> 
+  mutate(stream = tolower(stream),
+         year = year(date)) |>
+  filter(run %in% c("spring", NA, "not recorded")) |> 
+  group_by(year, passage_direction, stream) |>
+  summarise(count = sum(count, na.rm = T)) |> 
+  ungroup() |> 
+  pivot_wider(names_from = passage_direction, values_from = count) |> 
+  # calculate upstream passage for streams where passage direction is recorded
+  mutate(down = ifelse(is.na(down), 0, down),
+         up = case_when(stream %in% c("deer creek", "mill creek") ~ `NA`,
+                        !stream %in% c("deer creek", "mill creek") & is.na(up) ~ 0,
+                        TRUE ~ up)) |> 
+  select(-`NA`) |> 
+  group_by(year, stream) |> 
+  summarise(count = round(up - down), 0) |> 
+  select(year, count, stream) |> 
+  ungroup() |> 
+  glimpse()
+
+
+
+# holding
+holding <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_holding.csv",
+                                   bucket = gcs_get_global_bucket()))|> 
+  group_by(year, stream) |> 
+  summarise(count = sum(count, na.rm = T)) |> 
+  ungroup() |> 
+  glimpse()
+
+# redd
+redd <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_annual_redd.csv",
+                                bucket = gcs_get_global_bucket())) |> 
+  filter(run %in% c("spring", "not recorded")) |> 
+  # redds in these reaches are likely fall, so set to 0 for battle & clear
+  mutate(max_yearly_redd_count = case_when(reach %in% c("R6", "R6A", "R6B", "R7") & 
+                                             stream %in% c("battle creek", "clear creek") ~ 0,
+                                           TRUE ~ max_yearly_redd_count)) |> 
+  group_by(year, stream) |>
+  summarise(count = sum(max_yearly_redd_count, na.rm = T)) |> 
+  ungroup() |> 
+  select(year, stream, count) |> 
+  glimpse()
+
+redd_daily <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_daily_redd.csv",
+                                      bucket = gcs_get_global_bucket())) |> 
+  filter(run %in% c("spring", NA, "not recorded")) |> 
+  mutate(count = 1) |> 
+  group_by(year, stream) |> 
+  summarise(count = sum(count)) |> 
+  ungroup() |> 
+  glimpse()
+
+# carcass
+carcass <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_carcass.csv",
+                                   bucket = gcs_get_global_bucket())) |>
+  filter(run %in% c("spring", "unknown", NA)) |> 
+  select(date, stream, count) |> 
+  mutate(year = year(date)) |> 
+  group_by(year, stream) |> 
+  summarise(count = sum(count, na.rm = T)) |> 
+  ungroup() |> 
+  glimpse()
+
+# temperature
+
+# threshold
+# https://www.noaa.gov/sites/default/files/legacy/document/2020/Oct/07354626766.pdf 
+threshold <- 20
+
+# migratory temps - sac, months = 3:5
+# holding temps - trib specific; 5-7
+migratory_temp <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_temperature.csv",
+                                          bucket = gcs_get_global_bucket())) |> 
+  filter(stream == "sacramento river") |> 
+  filter(month(date) %in% 3:5) |> 
+  group_by(year(date)) |> 
+  mutate(above_threshold = ifelse(mean_daily_temp_c > threshold, TRUE, FALSE)) |> 
+  summarise(prop_days_exceed_threshold = round(sum(above_threshold, na.rm = T)/length(above_threshold), 2)) |> 
+  ungroup() |> 
+  mutate(prop_days_below_threshold = 1 - prop_days_exceed_threshold,
+         prop_days_below_threshold = ifelse(prop_days_below_threshold == 0, 0.001, prop_days_below_threshold)) |> 
+  rename(year = `year(date)`) |> 
+  glimpse()
+
+holding_temp <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_temperature.csv",
+                                        bucket = gcs_get_global_bucket())) |> 
+  filter(month(date) %in% 5:7) |> 
+  group_by(year(date), stream) |> 
+  mutate(above_threshold = ifelse(mean_daily_temp_c > threshold, TRUE, FALSE)) |> 
+  summarise(prop_days_exceed_threshold = round(sum(above_threshold, na.rm = T)/length(above_threshold), 2)) |> 
+  ungroup() |> 
+  mutate(prop_days_below_threshold = 1 - prop_days_exceed_threshold,
+         prop_days_below_threshold = ifelse(prop_days_below_threshold == 0, 0.001, prop_days_below_threshold)) |> 
+  rename(year = `year(date)`) |> 
+  glimpse()
+
+# linear model of prespawn mortality (upstream - redd) to get intercept
+upstream_passage_clear <- upstream_passage |> 
+  filter(stream %in% c("battle creek", "clear creek", "yuba river")) |> 
+  rename(upstream_count = count)
+
+redd_clear <- redd |> 
+  filter(stream %in% c("battle creek", "clear creek", "yuba river")) |> 
+  rename(redd_count = count)
+
+clear_prespawn <- upstream_passage_clear |> 
+  left_join(redd_clear, by = c("year", "stream")) |> 
+  mutate(female_upstream = upstream_count * 0.5,
+         prespawn_survival = redd_count / female_upstream,
+         prespawn_survival = ifelse(prespawn_survival > 1, 1, prespawn_survival)) |> 
+  glimpse()
+
+temp_prespawn <- left_join(clear_prespawn, migratory_temp |> 
+                             select(year, migratory_prop_days_exceed_threshold = prop_days_exceed_threshold),
+                           by = "year") |> 
+  left_join(holding_temp |> 
+              select(year, stream, holding_prop_days_exceed_threshold = prop_days_exceed_threshold)) |> 
+  mutate(total_prop_days_exceed_threshold = ifelse(is.na(holding_prop_days_exceed_threshold), migratory_prop_days_exceed_threshold, 
+                                                   holding_prop_days_exceed_threshold + migratory_prop_days_exceed_threshold)) |> 
+  glimpse()
+
+temp_prespawn |> 
+  ggplot(aes(x = total_prop_days_exceed_threshold, y = prespawn_survival, fill = stream)) + 
+  geom_point(aes(color = stream)) + geom_smooth(method = "lm") 
+
+m <- lm(data = temp_prespawn, prespawn_survival ~ total_prop_days_exceed_threshold + stream)
+summary(m)
+
+# TODO try median 7-day maximum temps (pull from gauges)
+
+surv_factor <- coef(m)[1] # TODO find estimates
+
+temp_prespawn_scaled <- temp_prespawn |> 
+  select(year, stream, prespawn_survival, total_prop_days_exceed_threshold) |> 
+  mutate(scaled_prop_days_exceed = total_prop_days_exceed_threshold * surv_factor) |> 
+  glimpse()
+
+temp_index <- temp_prespawn_scaled |> 
+  group_by(year) |> 
+  summarise(temp_index = mean(scaled_prop_days_exceed, na.rm = T)) |> 
+  ungroup() |> 
+  mutate(temp_index = ifelse(is.na(temp_index), 1, temp_index))
+
--- analysis/bayesian_adult_model.R
+++ analysis/bayesian_adult_model.R
@@ -1,163 +1,12 @@
 # implement modified Dauphin et al model
 
-
 # libraries ---------------------------------------------------------------
 library(tidyverse)
 library(lubridate)
-library(googleCloudStorageR)
 library(rstan)
 library(bayesplot)
 
-# pull adult data & process ----------------------------------------------------------------
-gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
-gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
-
-# upstream passage total counts
-upstream_passage <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_adult_upstream_passage.csv",
-                                            bucket = gcs_get_global_bucket())) |>
-  filter(!is.na(date)) |> 
-  mutate(stream = tolower(stream),
-         year = year(date)) |>
-  filter(run %in% c("spring", NA, "not recorded")) |> 
-  group_by(year, passage_direction, stream) |>
-  summarise(count = sum(count, na.rm = T)) |> 
-  ungroup() |> 
-  pivot_wider(names_from = passage_direction, values_from = count) |> 
-  # calculate upstream passage for streams where passage direction is recorded
-  mutate(down = ifelse(is.na(down), 0, down),
-         up = case_when(stream %in% c("deer creek", "mill creek") ~ `NA`,
-                        !stream %in% c("deer creek", "mill creek") & is.na(up) ~ 0,
-                        TRUE ~ up)) |> 
-  select(-`NA`) |> 
-  group_by(year, stream) |> 
-  summarise(count = round(up - down), 0) |> 
-  select(year, count, stream) |> 
-  ungroup() |> 
-  glimpse()
-  
-
-
-# holding
-holding <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_holding.csv",
-                                   bucket = gcs_get_global_bucket()))|> 
-  group_by(year, stream) |> 
-  summarise(count = sum(count, na.rm = T)) |> 
-  ungroup() |> 
-  glimpse()
-
-# redd
-redd <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_annual_redd.csv",
-                                bucket = gcs_get_global_bucket())) |> 
-  filter(run %in% c("spring", "not recorded")) |> 
-  # redds in these reaches are likely fall, so set to 0 for battle & clear
-  mutate(max_yearly_redd_count = case_when(reach %in% c("R6", "R6A", "R6B", "R7") & 
-                                             stream %in% c("battle creek", "clear creek") ~ 0,
-                                           TRUE ~ max_yearly_redd_count)) |> 
-  group_by(year, stream) |>
-  summarise(count = sum(max_yearly_redd_count, na.rm = T)) |> 
-  ungroup() |> 
-  select(year, stream, count) |> 
-  glimpse()
-
-redd_daily <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_daily_redd.csv",
-                                      bucket = gcs_get_global_bucket())) |> 
-  filter(run %in% c("spring", NA, "not recorded")) |> 
-  mutate(count = 1) |> 
-  group_by(year, stream) |> 
-  summarise(count = sum(count)) |> 
-  ungroup() |> 
-  glimpse()
-
-# carcass
-carcass <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_carcass.csv",
-                                   bucket = gcs_get_global_bucket())) |>
-  filter(run %in% c("spring", "unknown", NA)) |> 
-  select(date, stream, count) |> 
-  mutate(year = year(date)) |> 
-  group_by(year, stream) |> 
-  summarise(count = sum(count, na.rm = T)) |> 
-  ungroup() |> 
-  glimpse()
-
-# temperature
-
-# threshold
-# https://www.noaa.gov/sites/default/files/legacy/document/2020/Oct/07354626766.pdf 
-threshold <- 20
-
-# migratory temps - sac, months = 3:5
-# holding temps - trib specific; 5-7
-migratory_temp <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_temperature.csv",
-                                bucket = gcs_get_global_bucket())) |> 
-  filter(stream == "sacramento river") |> 
-  filter(month(date) %in% 3:5) |> 
-  group_by(year(date)) |> 
-  mutate(above_threshold = ifelse(mean_daily_temp_c > threshold, TRUE, FALSE)) |> 
-  summarise(prop_days_exceed_threshold = round(sum(above_threshold, na.rm = T)/length(above_threshold), 2)) |> 
-  ungroup() |> 
-  mutate(prop_days_below_threshold = 1 - prop_days_exceed_threshold,
-         prop_days_below_threshold = ifelse(prop_days_below_threshold == 0, 0.001, prop_days_below_threshold)) |> 
-  rename(year = `year(date)`) |> 
-  glimpse()
-
-holding_temp <- read_csv(gcs_get_object(object_name = "standard-format-data/standard_temperature.csv",
-                                          bucket = gcs_get_global_bucket())) |> 
-  filter(month(date) %in% 5:7) |> 
-  group_by(year(date), stream) |> 
-  mutate(above_threshold = ifelse(mean_daily_temp_c > threshold, TRUE, FALSE)) |> 
-  summarise(prop_days_exceed_threshold = round(sum(above_threshold, na.rm = T)/length(above_threshold), 2)) |> 
-  ungroup() |> 
-  mutate(prop_days_below_threshold = 1 - prop_days_exceed_threshold,
-         prop_days_below_threshold = ifelse(prop_days_below_threshold == 0, 0.001, prop_days_below_threshold)) |> 
-  rename(year = `year(date)`) |> 
-  glimpse()
-
-# linear model of prespawn mortality (upstream - redd) to get intercept
-upstream_passage_clear <- upstream_passage |> 
-  filter(stream %in% c("battle creek", "clear creek", "yuba river")) |> 
-  rename(upstream_count = count)
-
-redd_clear <- redd |> 
-  filter(stream %in% c("battle creek", "clear creek", "yuba river")) |> 
-  rename(redd_count = count)
-
-clear_prespawn <- upstream_passage_clear |> 
-  left_join(redd_clear, by = c("year", "stream")) |> 
-  mutate(female_upstream = upstream_count * 0.5,
-         prespawn_survival = redd_count / female_upstream,
-         prespawn_survival = ifelse(prespawn_survival > 1, 1, prespawn_survival)) |> 
-  glimpse()
-
-temp_prespawn <- left_join(clear_prespawn, migratory_temp |> 
-                             select(year, migratory_prop_days_exceed_threshold = prop_days_exceed_threshold),
-                           by = "year") |> 
-  left_join(holding_temp |> 
-              select(year, stream, holding_prop_days_exceed_threshold = prop_days_exceed_threshold)) |> 
-  mutate(total_prop_days_exceed_threshold = ifelse(is.na(holding_prop_days_exceed_threshold), migratory_prop_days_exceed_threshold, 
-                                                   holding_prop_days_exceed_threshold + migratory_prop_days_exceed_threshold)) |> 
-  glimpse()
-
-temp_prespawn |> 
-  ggplot(aes(x = total_prop_days_exceed_threshold, y = prespawn_survival, fill = stream)) + 
-  geom_point(aes(color = stream)) + geom_smooth(method = "lm") 
-
-m <- lm(data = temp_prespawn, prespawn_survival ~ total_prop_days_exceed_threshold + stream)
-summary(m)
-
-# TODO try median 7-day maximum temps (pull from gauges)
-
-surv_factor <- coef(m)[1] # TODO find estimates
-
-temp_prespawn_scaled <- temp_prespawn |> 
-  select(year, stream, prespawn_survival, total_prop_days_exceed_threshold) |> 
-  mutate(scaled_prop_days_exceed = total_prop_days_exceed_threshold * surv_factor) |> 
-  glimpse()
-
-temp_index <- temp_prespawn_scaled |> 
-  group_by(year) |> 
-  summarise(temp_index = mean(scaled_prop_days_exceed, na.rm = T)) |> 
-  ungroup() |> 
-  mutate(temp_index = ifelse(is.na(temp_index), 1, temp_index))
+source(here::here("analysis", "adult_model_data_prep.R"))
 
 
 # TODO input is prop_days_over_threshold * coefficient * ratio_k
@@ -299,6 +148,94 @@ mcmc_dens_overlay(test_fit, pars = c("mu_k", "sigma_k",  "mu_a", "sigma_a")) # s
 neff_ratio(test_fit, pars = c("mu_k", "sigma_k", "mu_a", "sigma_a")) # should be >0.1
 mcmc_acf(test_fit, pars = c("mu_k", "sigma_k", "mu_a", "sigma_a")) # should drop to be low
 rhat(test_fit, c("mu_k", "sigma_k", "mu_a", "sigma_a")) # should be close to 1
+
+
+# try missing data --------------------------------------------------------
+test_missing <- "
+  data {
+    int N;
+    int Y;
+    int upstream_count[N];
+    int redd_count[N];
+    int redd_missing[Y];
+    real ratio_k[N];
+    real temp_index[N];
+    real temp_index_old[Y];
+  }
+  parameters {
+    real <lower = 0> mu_k;
+    real <lower = 0> sigma_k;
+    real mu_a;
+    real <lower = 0> sigma_a;
+    real upstream_missing[Y];
+  }
+  model {
+    mu_k ~ gamma(3,1);
+    sigma_k ~ gamma(1,2);
+    mu_a ~ uniform(0, 1000);
+    sigma_a ~ gamma(0.001,0.001);
+    
+    for(i in 1:Y) {
+      upstream_missing[i] ~ uniform(0, 1000);
+    }
+    
+    real alpha[N];
+    real beta;
+    
+    beta = mu_k * sigma_k;
+
+    vector[N] lambda;
+    vector[Y] lambda_old;
+    vector[Y] ratio_k_old;
+    vector[Y] alpha_old;
+    
+    for(i in 1:N) { 
+      alpha[i] = mu_k * beta * temp_index[i];
+      upstream_count[i] ~ lognormal(mu_a, sigma_a);
+      ratio_k[i] ~ gamma(alpha[i], beta);
+      lambda[i] = upstream_count[i] * ratio_k[i];
+      redd_count[i] ~ poisson(lambda[i]);
+    }
+    
+    for(i in 1:Y){
+      alpha_old[i] = mu_k * beta * temp_index_old[i];
+      upstream_missing[i] ~ lognormal(mu_a, sigma_a);
+      ratio_k_old[i] ~ gamma(alpha_old[i], beta);
+      lambda_old[i] = upstream_missing[i] * ratio_k_old[i];
+      redd_missing[i] ~ poisson(lambda_old[i]);
+      
+    }
+
+  }"
+
+years_missing <- all_mill |> 
+  filter(is.na(upstream_count)) |> 
+  pull(year)
+Y <- length(years_missing)
+redd_missing <- all_mill |> 
+  filter(year %in% years_missing) |> 
+  pull(redd_count)
+
+temp_index_old <- temp_prespawn_scaled |> 
+  group_by(year) |> 
+  summarise(temp_index = mean(scaled_prop_days_exceed, na.rm = T)) |> 
+  ungroup() |> 
+  mutate(temp_index = ifelse(is.na(temp_index), 1, temp_index)) |> 
+  filter(year %in% years_missing) |> 
+  pull(temp_index) |> 
+  append(1, 0)
+
+test_missing_fit <- stan(model_code = test_missing, 
+                         data = list(N = N,
+                                     Y = Y,
+                                     upstream_count = upstream_count,
+                                     redd_count = redd_count,
+                                     redd_missing = redd_missing,
+                                     ratio_k = ratio_k,
+                                     temp_index = temp_index,
+                                     temp_index_old = temp_index_old), 
+                         #init = init_list,
+                         chains = 4, iter = 5000*2, seed = 84735)
 
 
 # dauphin et al model code ------------------------------------------------
