dcb0c1a5132dd3941b4d59a594f31dd449a5f54b
Erin Cain <ecain@flowwest.com>
2023-09-07 15:04:50 -0700

    Add google doc pull logic

--- analysis/accuracy_scoring.R
+++ analysis/accuracy_scoring.R
@@ -1,36 +1,51 @@
 library(tidyverse)
 library(googleCloudStorageR)
+library(googlesheets4)
 
-gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
-# # Set global bucket 
-gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
+# COMMENTED OUT, HOPING WE CAN USE DIRECT PULL FROM GOOGLE DOC------------------
+# gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
+# # # Set global bucket 
+# gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
+# 
+# # Load datasets ----------------------------------------------------------------
+# gcs_get_object(object_name = "sdm-resources/accuracy_scoring.xlsx",
+#                bucket = gcs_get_global_bucket(),
+#                saveToDisk = "analysis/accuracy_scoring.xlsx",
+#                overwrite = TRUE)
+# 
+# # Load in scoring sheet 
+# scores <- readxl::read_excel("analysis/accuracy_scoring.xlsx", 
+#                              sheet = 1,
+#                              col_types = c("text", "text", "text", 
+#                                            "logical", "logical", "logical", 
+#                                            "logical", "text", "text")) |> glimpse()
+# #### Warnings triggered by logical cells that are NA 
+# 
+# # Load in lookup sheet that maps submodels and tribs to approaches 
+# lookup <- readxl::read_excel("analysis/accuracy_scoring.xlsx", 
+#                              sheet = 2,
+#                              col_types = c("text", "text", "text", 
+#                                            "logical", "logical")) |> glimpse()
+# #### Warnings triggered by logical cells that are NA 
 
 
-# Load datasets ----------------------------------------------------------------
-gcs_get_object(object_name = "sdm-resources/accuracy_scoring.xlsx",
-               bucket = gcs_get_global_bucket(),
-               saveToDisk = "analysis/accuracy_scoring.xlsx",
-               overwrite = TRUE)
+#### Pull from google doc ------------------------------------------------------
+#### Working for me but left above code in case it isn't for you! 
+scores <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1i9-ItUZL8e_UG75Y54U9RGHYge93dXGoYvIYY_BTDN8/edit?usp=sharing", 
+                                    sheet = 7, 
+                                    col_types = "cccllllccc") |> 
+  janitor::clean_names() |> glimpse()
 
-# Load in scoring sheet 
-scores <- readxl::read_excel("analysis/accuracy_scoring.xlsx", 
-                             sheet = 1,
-                             col_types = c("text", "text", "text", 
-                                           "logical", "logical", "logical", 
-                                           "logical", "text", "text")) |> glimpse()
-#### Warnings triggered by logical cells that are NA 
+lookup <- googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1i9-ItUZL8e_UG75Y54U9RGHYge93dXGoYvIYY_BTDN8/edit?usp=sharing", 
+                                    sheet = 8,
+                                    col_types = "cccll") |> 
+  janitor::clean_names() |> glimpse()
 
-# Load in lookup sheet that maps submodels and tribs to approaches 
-lookup <- readxl::read_excel("analysis/accuracy_scoring.xlsx", 
-                             sheet = 2,
-                             col_types = c("text", "text", "text", 
-                                           "logical", "logical")) |> glimpse()
-#### Warnings triggered by logical cells that are NA 
 
 # Summarize and join data ------------------------------------------------------
 summarized_scores <- lookup |> 
   left_join(scores, by = c("tributary" = "tributary", "submodel" = "submodel")) |> 
-  pivot_longer(cols = single_annual_number:delta_timing, 
+  pivot_longer(cols = single_annual_number:delta_entry_timing, 
                names_to = "subobjective", 
                values_to = "needed_for_subobjective") |> glimpse()
 
