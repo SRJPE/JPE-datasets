4eb260f240d3f839400f2f511c19979e57b29ff3
Erin Cain <ecain@flowwest.com>
2023-09-07 09:39:57 -0700

    Adds google cloud path and comments

--- analysis/accuracy_scoring.R
+++ analysis/accuracy_scoring.R
@@ -1,23 +1,44 @@
 library(tidyverse)
+library(googleCloudStorageR)
 
+gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
+# # Set global bucket 
+gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
+
+
+# Load datasets ----------------------------------------------------------------
+gcs_get_object(object_name = "sdm-resources/accuracy_scoring.xlsx",
+               bucket = gcs_get_global_bucket(),
+               saveToDisk = "analysis/accuracy_scoring.xlsx",
+               overwrite = TRUE)
+
+# Load in scoring sheet 
 scores <- readxl::read_excel("analysis/acuracy_scoring.xlsx", 
                              sheet = 1,
                              col_types = c("text", "text", "text", 
                                            "logical", "logical", "logical", 
                                            "logical", "text", "text")) |> glimpse()
+#### Warnings triggered by logical cells that are NA 
+
+# Load in lookup sheet that maps submodels and tribs to approaches 
 lookup <- readxl::read_excel("analysis/acuracy_scoring.xlsx", 
                              sheet = 2,
                              col_types = c("text", "text", "text", 
                                            "logical", "logical")) |> glimpse()
+#### Warnings triggered by logical cells that are NA 
 
+# Summarize and join data ------------------------------------------------------
 summarized_scores <- lookup |> 
   left_join(scores, by = c("tributary" = "tributary", "submodel" = "submodel")) |> 
-  pivot_longer(cols = single_annual_number:delta_timing, names_to = "subobjective", values_to = "needed_for_subobjective") |> glimpse()
+  pivot_longer(cols = single_annual_number:delta_timing, 
+               names_to = "subobjective", 
+               values_to = "needed_for_subobjective") |> glimpse()
 
-View(summarized_scores)
-# Score for each approach & subcategory 
-# If current score = Low and n > 0 rank low 
-approach_name = "All Tributary RST S-R"
+View(summarized_scores) # quick visual inspection to make sure join looks reasonable 
+
+# Score for each approach & subcategory ----------------------------------------
+# If any scores = Low all low
+approach_name = "All Tributary RST S-R" # For stepping through function 
 produce_summarized_score <- function(approach_name){
   current_score <- summarized_scores |> 
     filter(approach == approach_name, 
@@ -39,6 +60,7 @@ produce_summarized_score <- function(approach_name){
   return(left_join(current_score, future_score))
 }
 
+# Produce sumarized scores for each tributary ----------------------------------
 # All tribs 
 produce_summarized_score("All Tributary RST S-R")
 produce_summarized_score("Early Outmigrant Trajectory - All Trib")
