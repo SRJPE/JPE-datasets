c57b2783682ad2834d82f8dbd98cae3955d0516b
alvizek <avizek@flowwest.com>
2023-10-01 14:11:02 -0700

    adds to gcs

--- data-raw/qc-markdowns/red_bluff_rst_data_prep.R
+++ data-raw/qc-markdowns/red_bluff_rst_data_prep.R
@@ -1,5 +1,12 @@
 # Pull in EDI data for Red Bluff
 library(EDIutils)
+library(tidyverse)
+library(googleCloudStorageR)
+
+gcs_auth(json_file = Sys.getenv("GCS_AUTH_FILE"))
+gcs_global_bucket(bucket = Sys.getenv("GCS_DEFAULT_BUCKET"))
+f <- function(input, output) write_csv(input, file = output)
+
 read_data_entity_names("edi.1365.7")
 # catch
 raw <- read_data_entity("edi.1365.7", "58540ac4ed34ce05f3309510f4be91e5")
@@ -42,6 +49,12 @@ rb_catch <- rb_catch_raw |>
   select(date, run, fork_length, lifestage, dead, count, stream, site, subsite,
          adipose_clipped, species, weight)
 
+gcs_upload(rb_catch,
+           object_function = f,
+           type = "csv",
+           name = "rst/rbdd/data/catch.csv",
+           predefinedAcl = "bucketLevel")
+
 rb_trap <- rb_trap_raw |> 
   mutate(stream = "sacramento river",
          site = "red bluff diversion dam",
@@ -59,6 +72,12 @@ rb_trap <- rb_trap_raw |>
   select(stream, site, subsite, trap_start_date, trap_start_time, trap_functioning,
          gear_type, is_half_cone_configuration, debris_volume, counter_start)
 
+gcs_upload(rb_trap,
+           object_function = f,
+           type = "csv",
+           name = "rst/rbdd/data/trap.csv",
+           predefinedAcl = "bucketLevel")
+
 rb_environmental_raw <- rb_trap_raw |> 
   mutate(stream = "sacramento river",
          site = "red bluff diversion dam",
@@ -75,6 +94,12 @@ rb_environmental <- rb_environmental_raw |>
   bind_rows(rb_environmental_raw |> 
               select(date, stream, site, subsite, diel, weather) |> 
               pivot_longer(cols = c(diel, weather), names_to = "parameter", values_to = "text"))
+
+gcs_upload(rb_environmental,
+           object_function = f,
+           type = "csv",
+           name = "rst/rbdd/data/environmental.csv",
+           predefinedAcl = "bucketLevel")
 
 summary_release_fish <- rb_release_fish_raw |> 
   mutate(source = toupper(source),
@@ -106,7 +131,13 @@ rb_release <- rb_release_raw |>
                                      T ~ source_released)) |> 
   select(stream, site, release_id, date_released, time_released, site_released,
          number_released, median_fork_length_released, run_released, origin_released,
-         source_released, turbidity_at_release) 
+         source_released, turbidity_at_release)
+
+gcs_upload(rb_release,
+           object_function = f,
+           type = "csv",
+           name = "rst/rbdd/data/release.csv",
+           predefinedAcl = "bucketLevel")
   
   
 rb_recapture <- rb_recapture_raw |> 
@@ -118,4 +149,10 @@ rb_recapture <- rb_recapture_raw |>
          number_recaptured = count) |> 
   select(stream, site, subsite, release_id, date_recaptured, number_recaptured,
          fork_length)
+
+gcs_upload(rb_recapture,
+           object_function = f,
+           type = "csv",
+           name = "rst/rbdd/data/recapture.csv",
+           predefinedAcl = "bucketLevel")
   
